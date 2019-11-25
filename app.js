cc74102d1a80460cabd5402b60b82f5bae33b03d
narendra mandiwal <nmandiwal@sapient.com>
2019-11-25 21:29:24 +0000

    node app saying hello world with hostname

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+var os = require("os");
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n from ' + os.hostname())
+}).listen(3000, '0.0.0.0');
