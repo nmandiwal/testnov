24bba5d5bc5c20bb217f11c93839bc1c17646811
narendra mandiwal <nmandiwal@sapient.com>
2019-11-27 11:45:55 +0000

    hello message change

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var os = require("os");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n from ' + os.hostname())
+  res.end('Hello World\n from machine ' + os.hostname())
 }).listen(3000, '0.0.0.0');
