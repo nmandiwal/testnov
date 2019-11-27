ef2c37ce02602b6de401357a35b86b4e4681e02d
narendra mandiwal <nmandiwal@sapient.com>
2019-11-27 12:00:50 +0000

    message updated

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var os = require("os");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n from machine ' + os.hostname())
+  res.end("Hi there! I'm being served from " + os.hostname())
 }).listen(3000, '0.0.0.0');
